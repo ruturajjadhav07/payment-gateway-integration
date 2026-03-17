b8d00dba30478945a04844e679d5295c42a379c5
“[github]” <ruturajjadhav122@gmail.com>
2026-03-17 11:59:08 +0530

    add logout button code

--- client/src/app/userComponent/LogoutButton.tsx
+++ client/src/app/userComponent/LogoutButton.tsx
@@ -23,7 +23,9 @@ export default function LogoutButton() {
   };
 
   return (
-    <button onClick={handleLogout} className=" px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 hover:shadow transition font-medium cursor-pointer" >
+    <button onClick={handleLogout} 
+    id="logoutButton"
+    className=" px-4 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600 hover:shadow transition font-medium cursor-pointer" >
     Logout
     </button>
       
